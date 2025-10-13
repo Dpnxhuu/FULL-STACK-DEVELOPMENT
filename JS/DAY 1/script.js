30d2f225cbec58a470e2b4bedaa7d2b5602d39d2
Deepanshu <dpnxhuu@gmail.com>
2025-10-13 17:13:49 +0530

    Day 1 learning JavaScript

--- JS/DAY 1/script.js
+++ JS/DAY 1/script.js
@@ -1,1 +1,78 @@
-console.log("hello world");
+// let student = {
+//     name :  "Deep",
+//     class : 12,
+//     rollNo : 21,
+// };
+
+// for(let key in student){
+//     console.log("key=", key, "value=", student[key]);
+// }
+// let str = "JavaScript";
+
+// let size = 0;
+// for(let i of str){
+//     console.log("i=", i);
+//     size++;
+// }
+
+// console.log("Size of string is = ", size);
+
+// for(let i = 0; i <= 100; i++)
+// {
+//     console.log("num=", i);
+// }
+
+// let name = "Deepanshu";
+// console.log(name[0]);
+
+// Template litreals in JavaScript
+
+// let str = "Dpnshuu";
+// let a = 10; 
+// let b = 22;
+
+// console.log("a = ", a, "and b =", b, "and str =" , str);
+// let output = `a = ${a} and b = ${b} and str = ${str}`;
+// console.log(output);
+
+// let string = "Dpnshuu";
+
+// // string = string.toUpperCase();
+// string = string.toLowerCase();
+// console.log(string);
+
+// let arr = [12,15,66,44,22,55,322,23,2344];
+
+// console.log(arr);
+// console.log(arr.length);
+
+// let arr = ["Deepanshu", "Abhinav" , "Deepak" , "Rishi"];
+
+// for(let i = 0; i < arr.length; i++)
+// {
+//     console.log(arr[i]);
+// }
+
+// 
+
+// let arr = ["tomatao", "Apple", "Pineapple"];
+
+// console.log(arr);
+
+// arr.push("Burger", "coocnut", "vinegar");
+
+// console.log(arr);
+
+// console.log(arr.toString());
+
+// arr.unshift("Rice", "vanila"); //add item from start
+
+// console.log(arr);
+
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();     // It delete a element from end
+// arr.shift();  //It delete element from start
+// console.log(arr);
+
