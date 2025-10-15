82432ab0bc583f4a97f5ffb87d82c18591b48623
Deepanshu <dpnxhuu@gmail.com>
2025-10-15 15:04:18 +0530

    Day 3 of learning javaScript

---
+++ JS/DAY 3/script.js
@@ -0,0 +1,11 @@
+// console.dir(window);
+// console.dir(document.body);
+// console.log(document.body);
+
+let val = document.getElementById("Main");
+
+console.log(val);
+
+let val2 = document.getElementsByClassName("Para");
+
+console.log(val2);
