4eeac9b9a183be079fcb87c9bad652e74b7d15c2
Deepanshu <dpnxhuu@gmail.com>
2025-10-16 22:13:28 +0530

    Day 4 of learning javascript

--- JS/DAY 3/script.js
+++ JS/DAY 3/script.js
@@ -2,10 +2,15 @@
 // console.dir(document.body);
 // console.log(document.body);
 
-let val = document.getElementById("Main");
+// let val = document.getElementById("Main");
 
-console.log(val);
+// console.log(val);
 
-let val2 = document.getElementsByClassName("Para");
+// let val2 = document.getElementsByClassName("Para");
 
-console.log(val2);
+// console.log(val2);
+// let a = 25;
+// for(let i = 0; i <= 10; i++)
+// {
+//     console.log(a);
+// }
---
+++ JS/DAY 4/script.js
@@ -0,0 +1,20 @@
+// console.log(Math.random());
+
+// let str = "Deepanshu";
+
+// let a = str.length();
+// console.log(a);
+
+
+const time = new Date();
+// console.log(time.toString());
+// console.log(time.toISOString());
+// console.log(time.toLocaleDateString());
+// console.log(time.toLocaleString());
+// console.log(time.toLocaleTimeString());
+console.log(time.getDate());
+console.log(time.getDay());
+console.log(time.getMonth());
+console.log(time.getFullYear());
+console.log(time.getSeconds());
+console.log(time.getHours());
