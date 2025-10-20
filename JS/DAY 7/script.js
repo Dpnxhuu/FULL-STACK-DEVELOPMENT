eda87774d4da0dd8650d3c4456a6dd83fc564fe5
Deepanshu <dpnxhuu@gmail.com>
2025-10-20 23:01:31 +0530

    Day 7 of learning Javascript

---
+++ JS/DAY 7/script.js
@@ -0,0 +1,39 @@
+// Linear search -->
+// let arr = [10,23,12,14,15,6,345,85,34,853,23,3542,2,34,32,4,34,];
+// // let n = prompt("Enter the number you want to search:");
+// let n = 23;
+// let index = [];
+// let m = 0;
+
+// for(let i = 0; i < arr.length; i++)
+// {
+//     if(n===arr[i])
+//     {
+//        console.log(`Value at index ${i}`);
+//        index[m] = i;
+//        m++;
+//     }
+//     // console.log("hello")
+    
+// }
+// console.log("Indices are:", index);
+
+// Delete value from an array -->
+// let arr = [10,23,12,14,15,6,345,85,34,853,23,3542,2,34,32,4,34,];
+
+// let n = 10;
+
+// console.log(`Size of arr:${arr.length} and elements are: ${arr}`);
+
+// for(let val = 0; val < arr.length; val++)
+// {
+//     if(arr[val] === n)
+//     {
+//         arr.splice(val,1);
+//         console.log(`Value that going to delete is: ${n} and index is ${val}`);
+//     }
+// }
+
+// console.log(`After deleting the size of arr: ${arr.length} and elements are: ${arr}`);
+
+  
