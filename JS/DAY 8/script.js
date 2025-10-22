ea531a85c79f033f12e570c2b5cde67e8bd24e14
Deepanshu <dpnxhuu@gmail.com>
2025-10-22 13:58:36 +0530

    Inserting in array

--- JS/DAY 8/script.js
+++ JS/DAY 8/script.js
@@ -1,12 +1,12 @@
-// To insert a new value in array -->
+// // To insert a new value in array -->
 // let data = [11,22,33,44,55,66,77,88,99];
-// let newEl = 0;
-// let position = 6;
+// let newEl = 111;
+// let position = 3;
 
 // console.log("Length: ",data.length);
 // for(let i = data.length-1; i >= 0; i--)
 // {
-//     if(i >= position)
+//     if((i >= position) && (position >= 0))
 //     {
 //         data[i+1] = data[i]
 //         if(i==position)
@@ -18,16 +18,16 @@
 // console.log(data);
 // console.log("Length: ",data.length);
 
-// To delet any value from array -->
+// // To delet any value from array -->
 
-let data = [10,20,30,40,50,60,70,80,90,100];
-let position = 4;
-console.log(`Before length: ${data.length}`);
-for(let i = position; i < data.length; i++)
-{
-    data[i] = data[i+1];
-}
-data.length = data.length-1;
-console.log(data);
-console.log(`After length: ${data.length}`);
+// let data = [10,20,30,40,50,60,70,80,90,100];
+// let position = 4;
+// console.log(`Before length: ${data.length}`);
+// for(let i = position; i < data.length; i++)
+// {
+//     data[i] = data[i+1];
+// }
+// data.length = data.length-1;
+// console.log(data);
+// console.log(`After length: ${data.length}`);
 
---
+++ JS/DAY 8/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+let data = [10,20,30,40,50,60,70,80,90,100];
+// let position = 4;
+// console.log(`Before length: ${data.length}`);
+// for(let i = position; i < data.length; i++)
+// {
+//     data[i] = data[i+1];
+// }
+// data.length = data.length-1;
+// console.log(data);
+// console.log(`After length: ${data.length}`);
+
