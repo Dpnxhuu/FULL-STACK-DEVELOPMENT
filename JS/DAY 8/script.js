2f060c6172cfdb8733044b5760e3103a3f7fd2a4
Deepanshu <dpnxhuu@gmail.com>
2025-10-22 13:31:43 +0530

    DAY 8 of learning js

---
+++ JS/DAY 8/script.js
@@ -0,0 +1,33 @@
+// To insert a new value in array -->
+// let data = [11,22,33,44,55,66,77,88,99];
+// let newEl = 0;
+// let position = 6;
+
+// console.log("Length: ",data.length);
+// for(let i = data.length-1; i >= 0; i--)
+// {
+//     if(i >= position)
+//     {
+//         data[i+1] = data[i]
+//         if(i==position)
+//         {
+//             data[position] = newEl;
+//         }
+//     }
+// }
+// console.log(data);
+// console.log("Length: ",data.length);
+
+// To delet any value from array -->
+
+let data = [10,20,30,40,50,60,70,80,90,100];
+let position = 4;
+console.log(`Before length: ${data.length}`);
+for(let i = position; i < data.length; i++)
+{
+    data[i] = data[i+1];
+}
+data.length = data.length-1;
+console.log(data);
+console.log(`After length: ${data.length}`);
+
