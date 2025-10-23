dc8ccdc4089c103a44b55648c6305b66bc45bea0
Deepanshu <dpnxhuu@gmail.com>
2025-10-23 18:54:27 +0530

    Day 9

--- JS/DAY 9/script.js
+++ JS/DAY 9/script.js
@@ -1,1 +1,36 @@
-console.log("hello world");
+// let data = [10,20,30,40,50];
+// data.reverse();
+// console.log(data);
+
+// let a =[1,2,3,4,5,6];
+// let b =[7,8,9,10,11,12];
+// let c = [...a,...b];
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// swaping two value with create third variable -->
+// let a = 10;
+// let b = 20;
+
+// a = a+b;
+// b = a-b;
+// a = a-b;
+
+// console.log(`a: ${a} b: ${b}`);
+
+// Method 2 of swaping value -->
+// let a = 10; 
+// let b = 20;
+
+// [a,b] = [b,a];
+// console.log(`a: ${a} b: ${b}`);
+
+// let a = 7;
+// let b = 2;
+
+// console.log(Math.floor(a/b));
+// console.log(a%b);
+
+// console.log(15>20 || 21>31 || 10<40)
