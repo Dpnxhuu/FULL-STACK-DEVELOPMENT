81e5a297e3891b84a1ee8a06a88611f042126460
Deepanshu <dpnxhuu@gmail.com>
2025-10-25 11:03:57 +0530

    Day 11 of learning js

---
+++ JS/DAY 11/script.js
@@ -0,0 +1,13 @@
+// const prompt = require('prompt-sync')();
+
+// // Taking user input
+// const name = prompt('What is your name? ');
+// console.log(`Hello, ${name}!`);
+
+// const prompt = require('prompt-sync')();
+
+// let a = prompt("Enter anything: ");
+// process.stdout.write(a);
+
+
+// process.stdout.write("Hello world");
