b23c3bb4962c9c5b04083ab3bdd4fa246cab64b3
Deepanshu <dpnxhuu@gmail.com>
2025-10-28 23:05:16 +0530

    attach about.txt file for acknowledge what I learn that day

--- JS/DAY 14/script.js
+++ JS/DAY 14/script.js
@@ -105,18 +105,17 @@ const prompt = require("prompt-sync")();
 // console.log(result);
 
 // Q.4 Frequency of each character -->
-let str = prompt("Enter any string:");
-let obj = {};
-for(let i = 0; i < str.length; i++)
-{
-    if(obj[str[i]] === undefined)
-    {
-        obj[str[i]] = 1;
-    }
-    else{
-        obj[str[i]] = obj[str[i]] + 1;
-    }
-}
-
-console.log(obj);
+// let str = prompt("Enter any string:");
+// let obj = {};
+// for(let i = 0; i < str.length; i++)
+// {
+//     if(obj[str[i]] === undefined)
+//     {
+//         obj[str[i]] = 1;
+//     }
+//     else{
+//         obj[str[i]] = obj[str[i]] + 1;
+//     }
+// }
+// console.log(obj);
 
