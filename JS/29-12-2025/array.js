a1bba44c53aea555471e5e7824c470b3d3eb3edd
Deepanshu <dpnxhuu@gmail.com>
2025-12-31 15:37:09 +0530

    current update

---
+++ JS/29-12-2025/array.js
@@ -0,0 +1,9 @@
+// Date: 29-12-2025
+// const prompt = require('prompt-sync')();
+
+// let arr = []; 
+// let a = 5;
+
+// let ar = new Array(5);
+// console.log(Array.isArray(5));
+// console.log(ar.length);
---
+++ JS/DAY 25/revision.js
@@ -0,0 +1,61 @@
+// Date: 28-12-2025
+const prompt = require('prompt-sync')();
+// console.log("Welcome home");
+
+// let radius = 14;
+// console.log(radius);
+
+// const student = {
+//     fullName : "Deepanshu",
+//     Add : "Meerut",
+//     age : 20,
+//     percentage : 84,
+//     cgpa : 8,
+// };
+
+// student["age"] = student["age"]+1;
+// console.log(student);
+
+// const profile = {
+//     username : 'SharadhaKhapra',
+//     post : 195,
+//     followers : "569k",
+//     following : 4,
+//     isFollow : false,
+// }
+
+// for(let key in profile)
+// {
+//     console.log(`Key = ${key} value = ${profile[key]}`);
+// }
+// // console.log(profile);
+
+// for(let i = 1; i <= 100; i++)
+// {
+//     if(i%2 === 0)
+//     {
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 1; i >= 0; i++)
+// {
+//     let a = Number(prompt("Enter any random number between 1 to 20: "));
+//     if(a === 7)
+//     {
+//         console.log("Congrats you entered the correct number!");
+//         break;
+//     }
+// }
+
+// let arr = [1,2,3,4,5,6];
+
+// // console.log(arr);
+// // console.log(arr.length);
+
+// for(let num of arr)
+// {
+//     console.log(num);
+// }
+
+let prices = [250,645,300,900,50];
