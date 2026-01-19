4fe02a2b6919e7f271078f40fa4e5e894daed5c6
Deepanshu <dpnxhuu@gmail.com>
2026-01-19 13:11:47 +0530

    learn what is react and how it actually works

---
+++ CPP/Day 1/hello.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int  main() {
+
+int age = 29;
+cout << age << endl;
+char grade = 'B';
+float PI = 3.1;
+cout << PI << endl;
+cout << grade << endl;
+
+}
